339c187c516f04b485095f261c421c313e42e3ea
Aditya Dutta <adidutta@protonmail.com>
2023-05-03 10:01:39 +0530

    daily backup

--- codes/mandelbrot.py
+++ codes/mandelbrot.py
@@ -1,2 +1,14 @@
-import numpy as np
-import matplotlib.pyplot as plt
+c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=21)
+
+members = get_members(c, num_iterations=20)
+
+
+plt.scatter(members.real, members.imag, color="black", marker=",", s=1)
+
+plt.gca().set_aspect("equal")
+
+plt.axis("off")
+
+plt.tight_layout()
+
+plt.show()
